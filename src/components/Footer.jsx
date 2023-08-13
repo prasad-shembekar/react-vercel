9bdf519e0c8c8aef81f803165aff7533f2179201
Prasad Shembekar <prasadmshembekar@gmail.com>
2023-08-13 16:42:17 +0530

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
       <div>
         <h5>Follow Us</h5>
         <div>
-          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+          <a href="https://youtube.com/prasadshembekar" target={"blank"}>
             Youtube
           </a>
-          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+          <a href="https://instagram.com/prasadshembekar" target={"blank"}>
             Instagram
           </a>
-          <a href="https://github.com/meabhisingh" target={"blank"}>
+          <a href="https://github.com/prasad-shembekar" target={"blank"}>
             Github
           </a>
         </div>
